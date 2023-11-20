d665dd77518e00511d122941b0287b7d15a4dc65
Emman-DG <mikhailemman.deguzman.cics@ust.edu.ph>
2023-11-21 03:00:12 +0800

    FrontEnd Nov 21

--- FinalProject/app/src/main/java/com/example/finalproject/Home.kt
+++ FinalProject/app/src/main/java/com/example/finalproject/Home.kt
@@ -16,8 +16,57 @@ class Home : AppCompatActivity() {
         startActivity(intent)
     }
 
-//    fun navigateToSearch(view: View) {
-//        val intent = Intent(this, Search::class.java);
-//        startActivity(intent)
-//    }
+    //Movie
+    fun navigateToMv1(view: View) {
+        val intent = Intent(this, Mv1::class.java);
+        startActivity(intent)
+    }
+    fun navigateToMv2(view: View) {
+        val intent = Intent(this, Mv2::class.java);
+        startActivity(intent)
+    }
+    fun navigateToMv3(view: View) {
+        val intent = Intent(this, Mv3::class.java);
+        startActivity(intent)
+    }
+    fun navigateToMv4(view: View) {
+        val intent = Intent(this, Mv4::class.java);
+        startActivity(intent)
+    }
+    fun navigateToMv5(view: View) {
+        val intent = Intent(this, Mv5::class.java);
+        startActivity(intent)
+    }
+    fun navigateToMv6(view: View) {
+        val intent = Intent(this, Mv6::class.java);
+        startActivity(intent)
+    }
+    fun navigateToMv7(view: View) {
+        val intent = Intent(this, Mv7::class.java);
+        startActivity(intent)
+    }
+    fun navigateToMv8(view: View) {
+        val intent = Intent(this, Mv8::class.java);
+        startActivity(intent)
+    }
+    fun navigateToMv9(view: View) {
+        val intent = Intent(this, Mv9::class.java);
+        startActivity(intent)
+    }
+
+    //Home
+    fun navigateToHome(view: View) {
+        val intent = Intent(this, Home::class.java);
+        startActivity(intent)
+    }
+    //Search
+    fun navigateToSearch(view: View) {
+        val intent = Intent(this, Search::class.java);
+        startActivity(intent)
+    }
+    //Profile
+    fun navigateToProfile(view: View) {
+        val intent = Intent(this, Profile::class.java);
+        startActivity(intent)
+    }
 }
---
+++ FinalProject/app/src/main/java/com/example/finalproject/Profile.kt
@@ -0,0 +1,28 @@
+package com.example.finalproject
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class Profile : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+    //Home
+    fun navigateToHome(view: View) {
+        val intent = Intent(this, Home::class.java);
+        startActivity(intent)
+    }
+    //Search
+    fun navigateToSearch(view: View) {
+        val intent = Intent(this, Search::class.java);
+        startActivity(intent)
+    }
+    //Profile
+    fun navigateToProfile(view: View) {
+        val intent = Intent(this, Profile::class.java);
+        startActivity(intent)
+    }
+}
